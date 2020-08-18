c16f2abe04629262bb5633d51fcb0af3dc69aee4
GSNCodes <gsnspidey@gmail.com>
2020-08-18 13:39:24 +0530

    August 18th

---
+++ August_LeetCode_Challenge/Numbers_With_Same_Consecutive_Difference.py
@@ -0,0 +1,49 @@
+Return all non-negative integers of length N such that the absolute difference 
+between every two consecutive digits is K.
+
+Note that every number in the answer must not have leading zeros except for the number 0 itself. 
+For example, 01 has one leading zero and is invalid, but 0 is valid.
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: N = 3, K = 7
+Output: [181,292,707,818,929]
+Explanation: Note that 070 is not a valid number, because it has leading zeroes.
+Example 2:
+
+Input: N = 2, K = 1
+Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]
+ 
+
+Note:
+
+1 <= N <= 9
+0 <= K <= 9
+
+
+class Solution:
+    def numsSameConsecDiff(self, N: int, K: int) -> List[int]:
+        current = [1,2,3,4,5,6,7,8,9,0]
+        
+        for i in range(N-1):
+            temp = []
+            # print(current)
+            for x in current:
+                
+                y = x%10 
+                
+                if x>0 and y+K<10:
+                    
+                    temp.append(x*10 + y+K)
+                    
+                if x>0 and K>0 and y-K>=0:
+                    
+                    temp.append(x*10 + y-K)
+                    
+            current = temp
+            
+        return current
