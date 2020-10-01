da099f53df3d98d3dabfef1c635ba2c8ec4c2805
SowmiyaNarayanan G <37922208+GSNCodes@users.noreply.github.com>
2020-10-01 13:08:03 +0530

    Update Number_Of_Recent_Calls.py

--- October_LeetCode_Challenge/Number_Of_Recent_Calls.py
+++ October_LeetCode_Challenge/Number_Of_Recent_Calls.py
@@ -25,6 +25,7 @@ recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 
 recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
 recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
 
+# O(1) Time and O(1) Space
 class RecentCounter:
 
     def __init__(self):
