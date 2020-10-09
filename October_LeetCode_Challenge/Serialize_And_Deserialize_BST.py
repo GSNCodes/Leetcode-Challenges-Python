e6a92c58ee7df551160026c3be8f05849a032f7f
GSNCodes <gsnspidey@gmail.com>
2020-10-09 13:39:54 +0530

    Oct 9

---
+++ October_LeetCode_Challenge/Serialize_And_Deserialize_BST.py
@@ -0,0 +1,80 @@
+Serialization is converting a data structure or object into a sequence of bits 
+so that it can be stored in a file or memory buffer, or transmitted across a network 
+connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. 
+There is no restriction on how your serialization/deserialization algorithm should work. 
+You need to ensure that a binary search tree can be serialized to a string, 
+and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Example 1:
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
+The input tree is guaranteed to be a binary search tree.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+
+
+# O(n) Time and O(n) Space
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        """
+        if root is None:
+            return 'x,'
+        
+        left_subtree  = self.serialize(root.left)
+        right_subtree = self.serialize(root.right)
+        
+        return str(root.val) + ',' + left_subtree + right_subtree
+
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        # print(data)
+        nodes = data.split(',')
+        # print(nodes)
+        
+        def helper(remaining_nodes):
+            temp = remaining_nodes.pop(0)
+            # print(temp)
+            if temp is 'x' or '':
+                return
+            node = TreeNode(temp)
+            node.left  = helper(remaining_nodes)
+            node.right = helper(remaining_nodes)
+            return node
+            
+        return helper(nodes)
+        
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
